563e385aff3f1e392a0d0068ef596dbb3e0b13dd
al5091 <angelovinolim@gmail.com>
2018-09-17 11:22:15 -0400

    Anagram!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,61 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner kbInput = new Scanner(System.in);
+        boolean length = false;
+        boolean characters = false;
+        System.out.println("Type in any word. Press enter. Type in another word that is an anagram of the first word.");
+        String word = kbInput.next();
+        System.out.println("Now type in the anagram.");
+        String anagram = kbInput.next();
+
+        int wordLength = word.length();
+        int anagramLength = anagram.length();
+
+        if (wordLength == anagramLength) {
+
+            length = true;
+
+        } else {
+
+            length = false;
+
+        }
+
+
+        if (length == false) {
+
+            System.out.println("That is not an anagram. Goodbye.");
+
+        } else {
+
+            char[] wordArray = word.toLowerCase().toCharArray();
+            char[] anagramArray = anagram.toLowerCase().toCharArray();
+
+            Arrays.sort(wordArray);
+            Arrays.sort(anagramArray);
+
+            if (wordArray == anagramArray) {
+
+                characters = true;
+
+            }
+
+        }
+
+        if (characters == true) {
+
+            System.out.println("That is an anagram! Congratulations!");
+
+        } else {
+
+            System.out.println("That is not an anagram. Goodbye.");
+
+        }
+
     }
 }
